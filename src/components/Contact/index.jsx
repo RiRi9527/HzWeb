164d8d633ed2aa9c9c33fdbd396c3bb39841919c
Hao Zhou <hzhou129527@gmail.com>
2023-10-22 22:10:31 -0400

    add: initial files

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,139 @@
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import 'leaflet/dist/leaflet.css'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_lnxsmik',
+        'template_dcn0xe8',
+        form.current,
+        'hmx-gHDSsxgzzKHlC'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
+  const position = [43.07, -83.47]
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in IT opportunities - especially on ambitious
+            orfadeInUp large projects. However, if you have any other requests
+            or questions, don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input
+                    placeholder="Name"
+                    type="text"
+                    name="user_name"
+                    required
+                  />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="user_email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="user_subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Hao Zhou,
+          <br />
+          USA
+          <br />
+          8104 N Webster Rd <br />
+          Mount Morris, MI <br />
+          <br />
+          <span>hzhou129527@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          {/* <MapContainer center={[44.96366, 19.61045]} zoom={13}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[44.96366, 19.61045]}>
+              <Popup>Sloba lives here, come over for a cup of coffee :)</Popup>
+            </Marker>
+          </MapContainer> */}
+          <MapContainer center={position} zoom={5} scrollWheelZoom={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Contact
---
+++ src/components/Home/Logo/index.jsx
@@ -0,0 +1,12 @@
+import LogoF from '../../../assets/images/family.png'
+import './index.scss'
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <img className="solid-logo" src={LogoF} alt="JavaScript,  Developer" />
+    </div>
+  )
+}
+
+export default Logo
